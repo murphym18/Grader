3db21a2f8eced8de3684e32a126e5c2bc09a68af
Michael Murphy <murphym18@gmail.com>
2015-04-01 21:13:42 -0700

    removed files that were moved to the implementation dir

--- model/admin/user.js
+++
@@ -1,9 +0,0 @@
-var IdentitySet = require('./identity').IdentitySet;
-
-function User(first, last) {
-   this.first = first;
-   this.last = last;
-   IdentitySet.call(this);
-}
-
-module.exports = User;
