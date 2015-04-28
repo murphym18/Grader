1486f9965db3510c06fc49f60aba3a9785b7d9a2
Michael Murphy <murphym18@gmail.com>
2015-04-27 22:27:22 -0700

    added nodemon config

---
+++ implementation/public/js/app/gradebook.js
@@ -0,0 +1,5 @@
+define(['jquery', 'backbone', 'backbone.marionette', 'app/app', 'handlebars'],function($, _, Backbone, Marionette, app, HandleBars){
+   var GradebookView= Marionette.CompositeView.extend({
+      template: "#leaf-branch-template"
+   })
+});
