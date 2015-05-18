1ea95c1686d35c823265aedc2a536475d8af5a0f
Michael Murphy <murphym18@gmail.com>
2015-05-18 05:08:15 +0000

    Added client-side unit testing

---
+++ implementation/public/js/test/main.js
@@ -0,0 +1,9 @@
+QUnit.test("hello test", function(assert) {
+   assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("courses test", function(assert) {
+   
+   
+   assert.ok(1 == "1", "Passed!");
+});
