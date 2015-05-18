595af551120abdbb031447fec0db948ed859f0ab
Scotty <psbeauch@gmail.com>
2015-05-18 16:48:44 -0700

    fixes

--- implementation/public/js/app/modifyAssignment.js
+++ implementation/public/js/app/modifyAssignment.js
@@ -1,4 +1,5 @@
 define(['app/app', 'text!templates/modifyAssignmentView.hbs', ], function(App, template) {
+    var currentAssignment;
 
     var ModifyAssignmentView = App.Mn.ItemView.extend({
         model: App.Assignment,
@@ -18,20 +19,57 @@ define(['app/app', 'text!templates/modifyAssignmentView.hbs', ], function(App, t
         },
         events : {
            'click @ui.modifyAssignmentButton' :  'showModifyAssignment',
-            'click @ui.ok' :  'closeModifyAssignment',
+            'click @ui.ok' :  'saveModifyAssignment',
             'click @ui.cancel' :  'closeModifyAssignment'
 
         },
-        updateAssignmentInfo : function() {
-
-        },
         showModifyAssignment : function() {
+            var ui = this.ui;
             this.ui.dialog.show();
             this.ui.modifyAssignmentButton.hide();
-            this.ui.assignmentName.val(this.model.get('categories')[1].assignments[0].name);
+            //this.ui.assignmentName.val(this.model.get('categories')[1].assignments[0].name);
             //this.ui.assignmentWeight.val(this.model.get('categories')[1].assignments[0].weight);
-            this.ui.assignmentTotalScore.val(this.model.get('categories')[1].assignments[0].rawPoints);
-            //this.ui.category.val();
+            //this.ui.assignmentTotalScore.val(this.model.get('categories')[1].assignments[0].rawPoints);
+            App.UserCourses.fetch().then( function() {
+                var course = App.UserCourses.at(0);
+
+                var categories = course.get('categories');
+
+                var catValues = [];
+                catValues.push('');
+                App._.forEach(categories, function(category) {
+                    catValues.push(category.name);
+                });
+
+                $.each(catValues, function(key, value) {   
+                     ui.category
+                         .append($("<option></option>")
+                         .text(value)); 
+                });
+                currentAssignment = categories[1].assignments[0].name;
+                ui.assignmentName.val(categories[1].assignments[0].name);
+                ui.assignmentTotalScore.val(categories[1].assignments[0].rawPoints);
+            });
+        },
+        saveModifyAssignment : function() {
+            var ui = this.ui;
+
+            App.UserCourses.fetch().then( function() {
+                var reqAssignName = currentAssignment;
+
+                var course = App.UserCourses.at(0);
+
+                var categories = course.get('categories');
+            
+                var assignment = $.grep(categories.assignments, function(e){ return e.name == reqAssignName; })[0];
+
+                assignment.name = ui.assignmentName.val();
+                category.rawPoints = ui.assignmentTotalScore.val();
+
+                course.set("categories", categories);
+                course.save();
+            });
+            this.closeModifyAssignment();
         },
         closeModifyAssignment : function() {
             this.ui.dialog.hide();
@@ -43,7 +81,7 @@ define(['app/app', 'text!templates/modifyAssignmentView.hbs', ], function(App, t
         //}
     })
 
-    App.Router.route("modifyAssingment", "home", function() {
+    App.Router.route("modifyAssignment", "home", function() {
         App.$.ajax({
             url: '/api/Courses'
         }).done(function (data) {
