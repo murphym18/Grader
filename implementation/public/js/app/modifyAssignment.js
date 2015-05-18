d2d5eb9610836e663cf4df77f8398368cba774f4
Scotty <psbeauch@gmail.com>
2015-05-18 12:42:19 -0700

    pulling from db
    ^^

--- implementation/public/js/app/modifyAssignment.js
+++ implementation/public/js/app/modifyAssignment.js
@@ -1,26 +1,37 @@
 define(['app/app', 'text!templates/modifyAssignmentView.hbs', ], function(App, template) {
 
-    var ModifyAssingmentView = App.Mn.ItemView.extend({
+    var ModifyAssignmentView = App.Mn.ItemView.extend({
         model: App.Assignment,
         template: App.Handlebars.compile(template),
         ui: {
             'modifyAssignmentButton' : '.modifyAssignmentButton',
             'ok' : '.ok',
             'cancel' : '.cancel',
-            'dialog' : '.popup-dialog'
+            'dialog' : '.popup-dialog',
+            'assignmentName' : '.assignmentName',
+            'assignmentWeight' : '.assignmentWeight',
+            'assignmentTotalScore' : '.assignmentTotalScore',
+            'category' : '.category'
         },
         onShow : function(){
             this.ui.dialog.hide();
         },
         events : {
-           'click @ui.modifyClassButton' :  'showModifyAssignment',
+           'click @ui.modifyAssignmentButton' :  'showModifyAssignment',
             'click @ui.ok' :  'closeModifyAssignment',
             'click @ui.cancel' :  'closeModifyAssignment'
+
+        },
+        updateAssignmentInfo : function() {
 
         },
         showModifyAssignment : function() {
             this.ui.dialog.show();
             this.ui.modifyAssignmentButton.hide();
+            this.ui.assignmentName.val(this.model.get('categories')[1].assignments[0].name);
+            //this.ui.assignmentWeight.val(this.model.get('categories')[1].assignments[0].weight);
+            this.ui.assignmentTotalScore.val(this.model.get('categories')[1].assignments[0].rawPoints);
+            //this.ui.category.val();
         },
         closeModifyAssignment : function() {
             this.ui.dialog.hide();
@@ -33,13 +44,17 @@ define(['app/app', 'text!templates/modifyAssignmentView.hbs', ], function(App, t
     })
 
     App.Router.route("modifyAssingment", "home", function() {
-        var modifyView = new ModifyAssignmentView();
-        App.show(modifyView);
-        //    layout = App.show(new App.StandardLayoutView());
-        //var mainView = new App.Marionette.ItemView({
-        //    template: App.Handlebars.compile(template)
-        //});
-        //layout.getRegion('main').show(mainView);
-        //layout.getRegion('header').show(new TopNavView);
+        App.$.ajax({
+            url: '/api/Courses'
+        }).done(function (data) {
+            var props = data[0];
+            props.url = '/api/Courses/' + props.colloquialUrl;
+            var course = new App.Backbone.Model(props);
+            console.dir(course);
+            var modifyView = new ModifyAssignmentView({
+                model: course
+            });
+            App.PopupRegion.show(modifyView);
+        });
     });
 });
