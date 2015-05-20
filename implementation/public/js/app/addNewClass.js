d2a5b80862986fb90d3dace4c6f8acec167ca40e
Michael Murphy <murphym18@gmail.com>
2015-05-20 22:48:58 +0000

    add new course dialog fixed

--- implementation/public/js/app/addNewClass.js
+++ implementation/public/js/app/addNewClass.js
@@ -3,7 +3,62 @@
  * @author Mike Ryu
  */
 define(['app/app', 'text!templates/addNewClassView.hbs' ], function(App, template) {
-
+    var defaultClass = {
+        "_id": ObjectID("555bf556790f01832ac30fe4"),
+        "classCode": "ART",
+        "classNumber": "388",
+        "section": "9",
+        "start": ISODate("2016-03-30T07:00:00.000Z"),
+        "end": ISODate("2016-06-12T07:00:00.000Z"),
+        "colloquialUrl": "ART-388-9-Spring2016",
+        "minCredit": 60,
+        "fColor": [
+            "rgba(255,0,0,0.8)",
+            "rgba(255,0,0,0.8)",
+            "rgba(255,0,0,0.9)",
+            "rgba(220,220,220,1)"
+        ],
+        "dColor": [
+            "rgba(255,0,0,0.5)",
+            "rgba(255,0,0,0.6)",
+            "rgba(255,0,0,0.7)",
+            "rgba(220,220,220,0.7)"
+        ],
+        "cColor": [
+            "rgba(255, 165, 0, 0.5)",
+            "rgba(255, 165, 0, 0.8)",
+            "rgba(255, 165, 0, 0.75)",
+            "rgba(255, 165, 0, 1)"
+        ],
+        "bColor": [
+            "rgba(255, 255, 0,0.5)",
+            "rgba(255, 255, 0,0.8)",
+            "rgba(255, 255, 0,0.75)",
+            "rgba(255, 255, 0,1)"
+        ],
+        "aColor": [
+            "rgba(0,255,0,0.5)",
+            "rgba(0,255,0,0.8)",
+            "rgba(0,255,0,0.75)",
+            "rgba(0,255,0,1)"
+        ],
+        "minD": 60,
+        "minC": 70,
+        "minB": 80,
+        "minA": 90,
+        "roles":[
+            {
+                "name":"NONE",
+                "_id":"555bf557790f01832ac31a5e",
+                "users":[],
+                "permissions":[]
+                
+            }
+        ],
+        "students": [],
+        "categories": [],
+        "__v": 0
+    };
     var AddNewClassView = App.Mn.ItemView.extend({
         model: App.Course,
         //model: App.UserCourses,
@@ -63,12 +118,13 @@ define(['app/app', 'text!templates/addNewClassView.hbs' ], function(App, templat
                 });
 
                 var course = new Course();
+                course.set(defaultClass);
                 course.set({
                     classCode: code,
                     classNumber: number,
                     section: '0',
-                    start: Date,
-                    end: Date,
+                    start: new Date(),
+                    end: new Date(),
                     year: '2015',
                     term: 'Spring',
                     colloquialUrl: code + '-' + number + '-0'
@@ -78,8 +134,8 @@ define(['app/app', 'text!templates/addNewClassView.hbs' ], function(App, templat
                 this.ui.addNewClassButton.show();
 
                 // Backbone.emulateHTTP = true;
-                // course.save();
-                this.model.save();
+                course.save();
+                //this.model.save();
             }
         },
 
@@ -102,12 +158,18 @@ define(['app/app', 'text!templates/addNewClassView.hbs' ], function(App, templat
 
     App.Router.route("addNewClass", "home", function() {
         App.UserCourses.fetch().then(function() {
-            var course = App.UserCourses.at(0);
-            //var course = App.UserCourses;
-            var addNewClassView = new AddNewClassView({
-                model: course
-            });
+            App.$.ajax({
+                url: '/api/Users/admin'
+            }).done(function(data) {
+                defaultClass.roles.users.push(data);
+                var course = App.UserCourses.at(0);
+                //var course = App.UserCourses;
+                var addNewClassView = new AddNewClassView({
+                    model: course
+                });
             App.PopupRegion.show(addNewClassView);
+            })
+            
         });
     });
 });
