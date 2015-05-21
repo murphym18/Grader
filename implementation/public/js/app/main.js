aaed62645f8fd4f5d13a8cc7985e2d2e7aefd666
Grant Campanelli <grantcampanelli@gmail.com>
2015-05-21 13:08:17 -0700

    Testing removed addNewClass

--- implementation/public/js/app/main.js
+++ implementation/public/js/app/main.js
@@ -36,7 +36,7 @@ requirejs.config({
 // Start the main app logic.
 
 
-requirejs(['app/app', 'app/login', 'app/home', 'app/chart', 'app/modifyAssignment', 'app/modifyCategory', 'app/newAssignment','app/modifyClass', 'app/modifyStudent', 'app/addNewClass', 'domReady!'], function(app) {
+requirejs(['app/app', 'app/login', 'app/home', 'app/chart', 'app/modifyAssignment', 'app/modifyCategory', 'app/newAssignment','app/modifyClass', 'app/modifyStudent', 'domReady!'], function(app) {
 
 
    app.start({});
