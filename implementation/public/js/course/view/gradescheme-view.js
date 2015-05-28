33a81e226b3d55513f7b130cc54907a1a17ce87f
Grant Campanelli <grantcampanelli@gmail.com>
2015-05-28 04:22:32 -0700

    Added a few more dialogs, late policy and grade scheme

---
+++ implementation/public/js/course/view/gradescheme-view.js
@@ -0,0 +1,43 @@
+define(function (require) {
+    var $ = require('jquery');
+    var _ = require('underscore');
+    var Backbone = require('util/backbone-helper');
+    var Hbs = require('handlebars');
+    var Mn = require('backbone.marionette');
+    var Q = require('q');
+    var Radio = require('backbone.radio');
+    var pageChannel = Radio.channel('page');
+    var courseChannel = Radio.channel('course');
+    var template = require('text!templates/gradeSchemeView.hbs');
+    var alertTemplate = require('text!templates/alert-block.hbs');
+
+    var Course = require('course/course');
+
+    return Mn.ItemView.extend({
+        tagName: 'div',
+        className: 'gradeScheme modal-dialog  modal-lg',
+        template: Hbs.compile(template),
+
+        ui: {
+
+        },
+
+        events: {
+            'change @ui.classCode': "updateClassCode",
+            'change @ui.classNumber': "updateClassNumber",
+            'change @ui.classSection': "updateClassSection",
+            'click @ui.winter': 'onSelectWinter',
+            'click @ui.spring': 'onSelectSpring',
+            'click @ui.summer': 'onSelectSummer',
+            'click @ui.fall': 'onSelectFall',
+            'change @ui.year': "onUpdateYear",
+            'click @ui.saveButton': 'onSaveCourse'
+        },
+
+        initialize: function(options) {
+
+
+        }
+
+    });
+});
---
+++ implementation/public/js/course/view/late-policy-view.js
@@ -0,0 +1,35 @@
+define(function (require) {
+    var $ = require('jquery');
+    var _ = require('underscore');
+    var Backbone = require('util/backbone-helper');
+    var Hbs = require('handlebars');
+    var Mn = require('backbone.marionette');
+    var Q = require('q');
+    var Radio = require('backbone.radio');
+    var pageChannel = Radio.channel('page');
+    var courseChannel = Radio.channel('course');
+    var template = require('text!templates/latePolicyView.hbs');
+    var alertTemplate = require('text!templates/alert-block.hbs');
+
+    var Course = require('course/course');
+
+    return Mn.ItemView.extend({
+        tagName: 'div',
+        className: 'latePolicy modal-dialog  modal-lg',
+        template: Hbs.compile(template),
+
+        ui: {
+
+        },
+
+        events: {
+
+        },
+
+        initialize: function(options) {
+
+
+        }
+
+    });
+});
