6e40656fc1b5ebe09fb3567cb852578128dfd8ed
Grant Campanelli <grantcampanelli@gmail.com>
2015-06-02 13:48:19 -0700

    Grade scheme now pulls in minimums

--- implementation/public/js/course/view/grade-scheme-view.js
+++ implementation/public/js/course/view/grade-scheme-view.js
@@ -19,7 +19,14 @@ define(function (require) {
         template: Hbs.compile(template),
 
         ui: {
-            'saveButton' : '.save'
+            'saveButton' : '.save',
+            'aMin' : '#grade-scheme-min-a',
+            'bMin' : '#grade-scheme-min-b',
+            'cMin' : '#grade-scheme-min-c',
+            'dMin' : '#grade-scheme-min-d',
+            'passMin' : '#grade-scheme-min-pass',
+            'buttonGraded' : '#grade-scheme-graded',
+            'buttonPassFail' : '#grade-scheme-pass-fail'
         },
 
         events: {
@@ -31,15 +38,36 @@ define(function (require) {
             'click @ui.summer': 'onSelectSummer',
             'click @ui.fall': 'onSelectFall',
             'change @ui.year': "onUpdateYear",
-            'click @ui.saveButton': 'onSaveCourse'
+            'click @ui.saveButton': 'onSaveGradeScheme'
         },
 
         initialize: function(options) {
+            this.model = new Course;
 
+        },
+        onShow : function() {
+
+            //this.model = this.model.course;
+            console.log(this.model.get('minA'));
+
+
+            var ui = this.ui;
+            var course = this.model;
+            console.log(course);
+
+            ui.aMin.val(course.get('minA'));
+            ui.bMin.val(course.get('minB'));
+            ui.cMin.val(course.get('minC'));
+            ui.dMin.val(course.get('minD'));
+            ui.passMin.val(course.get('minCredit'));
+        },
+        setNewMinimums : function () {
 
         },
 
-        onSaveCourse : function () {
+        onSaveGradeScheme : function () {
+            this.setNewMinimums();
+
             $('.cancel').click();
         }
 
